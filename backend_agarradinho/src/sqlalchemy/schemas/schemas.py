0b0ac46472ab4d51f08d8513c9f8a3cf32867b42
amandatsantos <tavaresamandasantos@hotmail.com>
2022-01-21 21:17:12 -0300

    agarradinho: schemas e sever adicionados

---
+++ backend_agarradinho/src/sqlalchemy/schemas/schemas.py
@@ -0,0 +1,31 @@
+from pydantic import BaseModel
+from typing import Optional, List
+
+class Usuario(BaseModel):
+    id : Optional[str]  = None
+    nome: str
+    telefone: str
+    
+
+class Produto(BaseModel):
+    id: Optional[str] = None
+    usuario:Usuario
+    nome:str
+    preco: float
+    disponivel: bool = False
+    descricao: str
+
+class Pedido(BaseModel):
+    id: Optional[str] = None
+    usuario: Usuario
+    produto: Produto
+    quatidade: int
+    entrega:bool = True
+    endereco: str
+    observacao: Optional[str] = 'Sem observações'
+
+class UsuarioMovi(BaseModel):
+    usuario: Usuario
+    vendas: List[Pedido]
+    produtos: List[Produto]
+    pedidos: List[Pedido]
